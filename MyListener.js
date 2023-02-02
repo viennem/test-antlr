9e6301797a42350f248d47d29c40e35f269d8300
X221782 <maxime.vienne-ext@socgen.com>
2023-02-02 17:34:03 +0100

    test

--- MyListener.js
+++ MyListener.js
@@ -2,12 +2,6 @@
 import myParserListener from './myParserListener.js';
 
 class MyListener extends myParserListener {
-  /**
-   * Default constructor.
-   *
-   * @param {FileInformation} fileInformation - File information.
-   * @param {ComponentDefinition[]} [definitions=[]] - All component definitions.
-   */
   constructor() {
     super();
   }
